66e4323747cf1e5198d11f7687aeb0e72240b48d
Marwa Ashraf <marwa@ashraf.com>
2023-03-19 16:29:06 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int num;
+	int alpha;
+
+	for (num = 48; num < 58 num++)
+	{
+		putchar(num);
+	}
+	for (alpha = 97; alpha < 103; alpha++)
+	{
+		putchar(alpha);
+	}
+	putchar('\n');
+	return (0);
+}
