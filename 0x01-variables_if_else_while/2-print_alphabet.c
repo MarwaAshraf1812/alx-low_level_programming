eea353939e73ee08039393a060d82f7f436a9bca
Marwa Ashraf <marwa@ashraf.com>
2023-03-19 14:09:29 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,11 +8,10 @@
  */
 int main(void)
 {
-	char alpha = 'a';
-	while(alpha <= 'z')
-	{
+	char alpha;
+	for (alpha= 'a'; alpha <= 'z'; alpha++)
 		putchar(alpha);
-	}
+
 	putchar('\n');
 
 	return (0);
