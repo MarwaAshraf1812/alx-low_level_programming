70111b192e69ed7f69dee61f6a5ab2104e57b6d2
Marwa Ashraf <marwa@ashraf.com>
2023-03-19 14:02:47 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - write lower and uppercase alph
+ *
+ * Return: 0 (success)
+ *
+ */
+int main(void)
+{
+	char alpha = 'a';
+	while(alpha <= 'z')
+	{
+		putchar(alpha);
+	}
+	putchar(\n);
+
+	return (0);
+}
