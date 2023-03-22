dec057728cc3de74329f5c72f55e4bfd090aedf5
Marwa Ashraf <marwa@ashraf.com>
2023-03-22 13:33:10 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_last_digit - function that prints the last digit
+ * @n: parameter
+ * Return: returns output
+ */
+
+int print_last_digit(int n)
+{
+	int digit;
+
+	if (n < 0)
+		digit = (n % 10) * -1;
+	else
+		digit = n % 10;
+	_putchar('0' + digit);
+	return (0);
+}
