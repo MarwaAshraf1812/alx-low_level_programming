d262616b9287c00926cae83e3efed87bc699a24c
Marwa Ashraf <marwa@ashraf.com>
2023-03-23 04:05:52 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _isupper -  checks for uppercase character.
+ * @c: c -  Variable
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+	for (c = 65; c <= 90; c++)
+	{
+		return (1);
+	}
+	return (0);
+}
