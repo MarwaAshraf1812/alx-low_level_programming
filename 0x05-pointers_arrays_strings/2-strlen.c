0eae9f895cba7616073a4ecbf6f46cdfb6149578
marwa ashraf <ashrafmarwa987@gmail.com>
2023-03-29 01:01:19 +0200

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strlen - function that returns the length of a string
+ * @s: parameter
+ * Return: string length
+ */
+int _strlen(char *s)
+{
+	int n;
+
+	for (n = 0; *s != '\0'; n++)
+		n++;
+	return (n);
