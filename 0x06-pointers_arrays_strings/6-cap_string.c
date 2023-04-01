fa21d349d9c42a6e9213407ff6ae2c28a1693956
Marwa Ashraf <marashraf090@gmail.com>
2023-03-31 19:41:13 -0700

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,37 +1,36 @@
 #include "main.h"
 /**
- *leet - encodes a string into 1337.
- *letters a and A are replaced by 4.
- *Letters e and E are replaced by 3
- *Letters o and O are replaced by 0
- *Letters t and T are replaced by 7
- *Letters l and L are replaced by 1
+ *cap_string - capitalizes every first letter of a word in a string.
+ *separators of words are:  space, tabulation,
+ * new line, ,, ;, ., !, ?, ", (, ), {, and }.
  *@s: pointer to string.
  *
  *Return: pointer to s.
  */
-char *leet(char *s)
+char *cap_string(char *s)
 {
-	int stringCount, leetCount;
-	char leetLetters[] = "aAeEoOtTlL";
-	char leetNums[] = "4433007711";
+	int count;
 
 /*  scan through string */
-	stringCount = 0;
-	while (s[stringCount] != '\0')
-/* check whether leetLetter is found */
-	{
-		leetCount = 0;
-		while (leetCount < 10)
+	count = 0;
+	while (s[count] != '\0')
+	{/* if next character after count is a char , capitalise it */
+		if (s[0] >= 97 && s[0] <= 122)
 		{
-			if (leetLetters[leetCount] == s[stringCount])
-			{
-				s[stringCount] = leetNums[leetCount];
-			}
-			leetCount++;
+			s[0] = s[0] - 32;
 		}
-		stringCount++;
+		if (s[count] == ' ' || s[count] == '\t' || s[count] == '\n'
+		    || s[count] == ',' || s[count] == ';' || s[count] == '.'
+		    || s[count] == '.' || s[count] == '!' || s[count] == '?'
+		    || s[count] == '"' || s[count] == '(' || s[count] == ')'
+		    || s[count] == '{' || s[count] == '}')
+		{
+			if (s[count + 1] >= 97 && s[count + 1] <= 122)
+			{
+				s[count + 1] = s[count + 1] - 32;
+			}
+		}
+		count++;
 	}
 	return (s);
 }
-
