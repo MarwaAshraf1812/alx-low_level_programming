5d30a77701b531655e5be09e4ae3f589c8cd3e43
Marwa Ashraf <marashraf090@gmail.com>
2023-03-31 19:06:01 -0700

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,7 +13,7 @@ int _strcmp(char *s1, char *s2)
 	int cmp, diff;
 
 	cmp = 0;
-	while (s1[cmp] == s2[cmp] && s1[cmp] != '\0';)
+	while (s1[cmp] == s2[cmp] && s1[cmp] != '\0')
 		cmp++;
 	diff = s1[cmp] - s2[cmp];
 	return (diff);
