0f74a8b45802c0f4185a792fe4de8bbb9536720d
Marwa Ashraf <marashraf090@gmail.com>
2023-04-08 06:17:00 -0700

    Task 0

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+void print_chessboard( char (*a)[8]);
+int _putchar(char c);
+void set_string(char **s, char *to);
