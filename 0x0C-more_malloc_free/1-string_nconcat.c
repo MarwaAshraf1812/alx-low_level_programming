a654515b92919a59c422d2e50b3ed5f9dd169dd6
Marwa Ashraf <marashraf090@gmail.com>
2023-04-14 20:02:50 -0700

    Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,9 +16,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int s1_len, s2_len, i, j;
 
 	if (s1 == NULL)
-		s1 = " ";
+		s1 = "";
 	if (s2 == NULL)
-		s2 = " ";
+		s2 = "";
 
 	for (s1_len = 0; s1[s1_len] != '\0'; s1_len++)
 		;
