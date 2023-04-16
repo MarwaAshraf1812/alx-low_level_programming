9241e3d077a5e0e8e090583680eb6886d0ec8f7b
Marwa Ashraf <marashraf090@gmail.com>
2023-04-15 22:34:06 -0700

    Task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,5 +8,16 @@
  */
 int main(int argc, char *argv[])
 {
-	int a, b;
-	int  
+	int sum;
+
+	if (argc == 3)
+	{
+		sum = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
