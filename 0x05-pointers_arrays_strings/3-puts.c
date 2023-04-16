0c831c3e2ff0c0554e3eef0a29b5dd9c205e620b
Marwa Ashraf <marashraf090@gmail.com>
2023-04-15 19:15:28 -0700

    Task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,13 +1,16 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * _puts - Entry point.
- * @str: pointer
- *
-*/
-
+  * _puts - Prints a string
+  * @str: The string to print
+  *
+  * Return: void
+  */
 void _puts(char *str)
 {
-puts(str);
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+
+	_putchar('\n');
 }
