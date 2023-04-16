2090e4db5ed8132dbcbd953ce56a9599cf4be262
Marwa Ashraf <marashraf090@gmail.com>
2023-04-15 22:25:51 -0700

    Task 2

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print producet of argument
+ * @argc: number of argument
+ * @argv: array of argument
+ * Return: 0 on success, 1 if not
+ */
+int main(int argc, char *argv[])
+{
+	int a, b;
+	int  
