72eed37fef2ddcbe8037f477eb137e0d569e48bb
Marwa Ashraf <marashraf090@gmail.com>
2023-04-19 21:15:01 -0700

    Task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -18,7 +18,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 		{
 			if (cmp(array[i]))
 				break;
-			return (-1);
+			if (!cmp(array[size - 1]))
+				return (-1);
 		}
 		return (i);
 	}
