8e02b0ba5235e13cb91080714d791a35ac9d93c0
Marwa Ashraf <marashraf090@gmail.com>
2023-04-19 20:06:10 -0700

    Task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -8,16 +8,20 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i = 0;
+	int i;
 
-	if (array && size && cmp)
+	if (array != NULL && cmp != NULL)
 	{
-		while (i < size)
+		if (size <= 0)
+			return (-1);
+		for (i = 0; i < size; i++)
 		{
 			if (cmp(array[i]))
-				return (i);
-			i++;
+				break;
+			else
+				return (-1);
 		}
-	return (-1);
+		return (i);
 	}
+	return (-1);
 }
