7610e2401b3bfc640c45c8ef59844e0b2a19f92e
Marwa Ashraf <marashraf090@gmail.com>
2023-04-19 17:03:19 -0700

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a struct representing a dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner ot dog
+ */
+
+struct dog {
+	char name;
+	float age;
+	char owner;
+};
+
+#endif
