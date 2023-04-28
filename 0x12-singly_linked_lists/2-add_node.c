091494eda5ed363e6e7d4d57e01792bc65d9d7dd
Marwa Ashraf <marashraf090@gmail.com>
2023-04-27 17:21:30 -0700

    Task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,23 +10,22 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node = malloc(sizeof(list_t));
 	size_t i;
 
-	if (head == NULL || str == NULL)
-		return (NULL);
+	list_t *new_node = malloc(sizeof(list_t));
 
 	if (new_node == NULL)
+	{
 		return (NULL);
+	}
 
 	new_node->str = strdup(str);
 
 	for (i = 0; str[i] != '\0'; i++)
 		;
-
-	new_node->len = strlen(len);
+	new_node->len = i;
 	new_node->next = *head;
-	*head = new_node;
 
+	*head = new_node;
 	return (new_node);
 }
