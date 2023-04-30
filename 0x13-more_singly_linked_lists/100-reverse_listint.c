e79e870299383340632964ad5ff07587eb2e1895
Marwa Ashraf <marashraf090@gmail.com>
2023-04-29 19:08:19 -0700

    Task 11

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -9,17 +9,17 @@
 listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *prev = NULL;
+	listint_t *current = NULL;
 	listint_t *next = NULL;
 
-	while (*head)
+	while (*head != NULL)
 	{
 		next = (*head)->next;
 		(*head)->next = prev;
 		prev = *head;
-		*head = next;
+		current = next;
+		*head = current;
 	}
-
 	*head = prev;
-
 	return (*head);
 }
