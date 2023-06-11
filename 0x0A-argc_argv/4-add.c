7cf76006eea0a36eeeffaa52bdcbe430bd98007c
Marwa Ashraf <marashraf090@gmail.com>
2023-06-11 15:01:09 -0700

    Task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds positive numbers
+ * @argc: number of argument passed
+ * @argv: array of argument
+ * Return: 1 if errors, 0 if success
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	char *c;
+
+	while (--argc)
+	{
+		for (c = argv[argc]; *c; c++)
+		{
+			if (*c < '0' && *c > '9')
+			{
+				return (printf("Error\n"), 1);
+			}
+			sum += atoi(argv[argc]);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
