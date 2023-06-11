762d1c791206bd13f6deb03be2b99382ae90fd3b
Marwa Ashraf <marashraf090@gmail.com>
2023-06-11 13:36:42 -0700

    Task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 /**
  * main - function main
  * @argc: counter
@@ -9,6 +8,6 @@
 int main(int argc, char *argv[])
 {
 	if (argc > 0)
-		printf("%s \n", argv[0]);
-	exit(EXIT_SUCCESS);
+		printf("%s\n", argv[0]);
+	return (0);
 }
