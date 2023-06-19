0a8b8ef53f434fcbf4e234883408bc37e036c62c
Marwa Ashraf <marashraf090@gmail.com>
2023-06-19 10:47:43 -0700

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - a struct representing a dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner ot dog
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
