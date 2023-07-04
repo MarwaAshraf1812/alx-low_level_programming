9bf2ccccb22f75deb4180e1bc92fb45db260a41e
MarwaAshraf1812 <marashraf090@gmail.com>
2023-07-04 16:33:15 -0700

    Free 2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,16 +8,16 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *current;
+        listint_t *current;
 
-	if (head == NULL)
-		return ;
-	current = *head;
-	while (current)
-	{
-		current = current->next;
-		free(current);
+        if (head == NULL)
+                return ;
+        current = *head;
+        while (current)
+        {
+                current = current->next;
+                free(current);
 
-	}
-	*head = NULL;
+        }
+        *head = NULL;
 }
