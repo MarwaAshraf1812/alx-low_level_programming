3943e4b317d171657a1fb2451c394f4e643c9a9b
MarwaAshraf1812 <marashraf090@gmail.com>
2023-09-08 15:16:35 -0700

    Task 0 :  >>> ht = {}

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,6 +9,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table = malloc(sizeof(hash_table_t));
+
 	if (table == NULL)
 	{
 		return (NULL);
@@ -16,7 +17,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	table->size = size;
 	hash_node_t *bucket = malloc(sizeof(hash_node_t) * size);
-	if (bucket == NULL)
+
+	{
+		if (bucket == NULL)
 	{
 		free(table);
 		return (NULL);
