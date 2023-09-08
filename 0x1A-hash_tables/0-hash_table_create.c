071e4a4704769f573efd2b3fe10ce0020b969db6
MarwaAshraf1812 <marashraf090@gmail.com>
2023-09-09 00:47:39 +0300

    Task 0 : >>> ht = {}

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - Create hash table
+ * @size: Size of hash Table
+ * 
+ * Return: pointer to the hash table
+*/
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    hash_table_t *table = malloc(sizeof(hash_table_t));
+    if (table == NULL)
+    {
+        return (NULL);
+    }
+        
+    table->size = size;
+    hash_node_t *bucket = malloc(sizeof(hash_node_t) * size);
+    if (bucket == NULL)
+    {
+        free(table);
+        return (NULL);
+    }
+    table->array = bucket;
+    return (table);
+}
