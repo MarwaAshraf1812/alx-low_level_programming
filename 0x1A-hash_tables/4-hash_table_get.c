040f0bd88da56bf0db125d817aca537d5c14ce61
MarwaAshraf1812 <marashraf090@gmail.com>
2023-09-09 05:30:14 +0300

    Task 4 : >>> ht['betty']

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,13 +10,22 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index = 0;
+	hash_node_t *current_node;
 
 	if (ht == NULL || key == NULL)
 		return (NULL);
 
 	index = key_index((unsigned char *)key, ht->size);
+	current_node = ht->array[index];
 
-	if (ht->array[index])
-		return ht->array[index]->value;
+
+	while (current_node != NULL)
+	{
+		if (strcmp(current_node->key, key) == 0)
+		{
+			return (current_node->value);
+		}
+		current_node = current_node->next;
+	}
 	return (NULL);
 }
