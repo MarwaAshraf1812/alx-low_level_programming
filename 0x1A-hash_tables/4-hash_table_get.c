f0ed9f07beaf2dadcb19a1982d45b5e539fc2f2e
MarwaAshraf1812 <marashraf090@gmail.com>
2023-09-09 05:27:17 +0300

    Task 4 : >>> ht['betty']

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,6 +1,6 @@
 #include "hash_tables.h"
 /**
- * hash_table_set - Adds an element to the hash table
+ * hash_table_get - Adds an element to the hash table
  * @ht: the hash table
  * @key: Key
  *
@@ -10,22 +10,13 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index = 0;
-	hash_node_t *current_node;
 
 	if (ht == NULL || key == NULL)
 		return (NULL);
 
 	index = key_index((unsigned char *)key, ht->size);
-	current_node = ht->array[index];
 
-
-	while (current_node != NULL)
-	{
-		if (strcmp(current_node->key, key))
-		{
-			return (current_node->value);
-		}
-		current_node = current_node->next;
-	}
+	if (ht->array[index])
+		return ht->array[index]->value;
 	return (NULL);
 }
