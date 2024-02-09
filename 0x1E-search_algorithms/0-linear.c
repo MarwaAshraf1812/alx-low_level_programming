790909d38056825dd17dff2dac9e45dcfc189a9e
MarwaAshraf1812 <marashraf090@gmail.com>
2024-02-09 22:47:11 +0200

    Task 0: Linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -6,23 +6,23 @@
  * - *array : is a pointer to the first element
  * - size : size of array
  * - value : is the value to search for
- * 
+ *
  * Return :  the first index where value is located
 */
 
 
 int linear_search(int *array, size_t size, int value)
 {
-    if (array == NULL)
-    {
-        return (-1);
-    }
-    for (size_t i = 0;  i < size; ++i) 
-    {
-        if (array[i] == value)
-        {
-            return (i);
-        }
-    }
-    return (-1);
+	if (array == NULL)
+	{
+		return (-1);
+	}
+	for (size_t i = 0;  i < size; ++i)
+	{
+		if (array[i] == value)
+		{
+			return (i);
+		}
+	}
+	return (-1);
 }
